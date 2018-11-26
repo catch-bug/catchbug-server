3623133e3b06d86b163d46bb81977d7d04805b07
mira <ms502040@gmail.com>
2018-11-26 02:26:37 +0100

    add trace_chain into api/item

--- api/v1.0/item.php
+++ api/v1.0/item.php
@@ -5,6 +5,7 @@
  * @Date   : 7.11.18
  * @Time   : 20:28
  */
+use \rollbug\itemWriter;
 
 ob_start();
 require_once __DIR__ . '/../../inc/mysqli.php';
@@ -14,111 +15,101 @@ require_once __DIR__ . '/../../inc/mysqli.php';
 switch ($method){
   case 'POST':
 
-    $uuid = $payload->data->uuid;
+    if ($payload !== null) {
 
-
-    $stmt = $mysqli->prepare('select user.id, p.last_item, p.id, t.type from user join project p on user.id = p.user_id join token t on p.user_id = t.user_id and p.id = t.project_id where t.token=? and FIND_IN_SET(\'post_server_item\',t.type)>0');
-    $stmt->bind_param('s', $access_token);
-    $stmt->bind_result($userId, $last_item, $projectId, $tokenType);
-    $stmt->execute();
-    $mysqli_error .= "\n" . $mysqli->error;
-    $stmt->fetch();
-    $stmt->close();
-
-    if ($userId !== null) {
-
-      $query_success = true;
-
-      $mysqli->autocommit(false);
-      $mysqli_error = '';
-
-      switch ($payload->data->language) {
-        case 'php':
-          $id_str = $payload->data->body->trace->frames[0]->filename . '|'
-              . $payload->data->body->trace->frames[0]->lineno . '|'
-              . $payload->data->body->trace->exception->class . '|'
-              . $payload->data->body->trace->exception->message;
-          break;
-
-      }
-
-      // search for item in db
-      $stmt = $mysqli->prepare('select item.id, item.last_occ from item where user_id=? and project_id=? and id_str=?');
-      $stmt->bind_param('iis', $userId, $projectId, $id_str);
-      $stmt->bind_result($itemId, $last_occ);
-      $query_success = $query_success && $stmt->execute();
-      $mysqli_error .= "\n" . $mysqli->error;
+      $stmt = $mysqli->prepare('select user.id, p.last_item, p.id, t.type from user join project p on user.id = p.user_id join token t on p.user_id = t.user_id and p.id = t.project_id where t.token=?');
+      $stmt->bind_param('s', $access_token);
+      $stmt->bind_result($userId, $last_item, $projectId, $tokenType);
+      $stmt->execute();
+    //  $mysqli_error .= "\n" . $mysqli->error;
       $stmt->fetch();
       $stmt->close();
 
-      $date = new DateTime('@' . $payload->data->timestamp);
-      $timestamp = $date->format('Y-m-d H:i:s');
+      if ($userId !== null) {
+        $mysqli->autocommit(false);
 
-      // if new item
-      if ($itemId === null) {
-        $itemId = $last_item + 1;
-        $stmt = $mysqli->prepare('INSERT INTO item (id, user_id, project_id, level, language, id_str, last_occ, last_timestamp) VALUES (?,?,?,?,?,?,1, ?);');
-        $stmt->bind_param('iiissss', $itemId, $userId, $projectId, $payload->data->level, $payload->data->language, $id_str, $timestamp);
-        $query_success = $query_success && $stmt->execute();
-        $mysqli_error .= "\n" . $mysqli->error;
-        $stmt->fetch();
-        $stmt->close();
+        require_once __DIR__ . '/../../inc/itemWriter.php';
 
-        $stmt = $mysqli->prepare('UPDATE project SET last_item = last_item + 1 WHERE user_id=? and id=?');
-        $stmt->bind_param('ii', $userId, $projectId);
-        $query_success = $query_success && $stmt->execute();
-        $mysqli_error .= "\n" . $mysqli->error;
-        $stmt->close();
+        $itemWriter = new itemWriter($mysqli, $config);
 
-        $last_occ = 1;
-      } else {
-        $stmt = $mysqli->prepare('UPDATE item SET last_occ = last_occ + 1, last_timestamp=? WHERE user_id=? and project_id=? and id=?');
-        $stmt->bind_param('siii', $timestamp, $userId, $projectId, $itemId);
-        $query_success = $query_success && $stmt->execute();
-        $mysqli_error .= "\n" . $mysqli->error;
-        $stmt->close();
+        // todo check /  switch server/client token
 
-        $last_occ++;
-      }
 
-      if ($last_occ <= $config->max_occurences) {
-        $data = json_encode($payload->data);
-        $stmt = $mysqli->prepare('INSERT INTO occurence (id, user_id, project_id, item_id, timestamp, data) VALUES (?,?,?,?,?,?);');
-        $stmt->bind_param('iiiiss', $last_occ, $userId, $projectId, $itemId, $timestamp, $data);
-        $query_success = $query_success && $stmt->execute();
-        $mysqli_error .= "\n" . $mysqli->error;
-        $stmt->fetch();
-        $stmt->close();
-      }
+        if (property_exists($payload->data->body, 'trace_chain')) {
+          $itemWriter->setItemType('trace');
+          $itemWriter->setUserId($userId);
+          $itemWriter->setProjectId($projectId);
+          $itemWriter->setPayload($payload);
+          $itemWriter->setLastItem($last_item);
 
-      /*
-      $fp = fopen(__DIR__ . "/mysqli.txt", 'wb');
-      fwrite($fp, $mysqli_error);
-      fclose($fp);
-       */
+          foreach ($payload->data->body->trace_chain as $trace){
+            $id_str = $itemWriter->makeIdStr($trace);
+            $itemWriter->setIdStr($id_str);
 
-      if ($query_success) {
-        if ($mysqli->commit()) {
-          header('Content-Type: application/json; charset=utf-8');
-          http_response_code(200);
-          echo "{err: 0,result: {uuid: \"$uuid\"}}";
+            $itemWriter->writeItem(true);
+          }
+
+        } elseif (property_exists($payload->data->body, 'trace')) {
+          $itemWriter->setItemType('trace');
+          $itemWriter->setUserId($userId);
+          $itemWriter->setProjectId($projectId);
+          $itemWriter->setPayload($payload);
+          $itemWriter->setLastItem($last_item);
+
+          $id_str = $itemWriter->makeIdStr($payload->data->body->trace);
+          $itemWriter->setIdStr($id_str);
+
+          $itemWriter->writeItem();
+
+        } elseif (property_exists($payload->data->body, 'message')) {
+          $itemWriter->setItemType('message');
+          $id_str = ' | |' . $payload->data->level . '|' . $payload->data->body->message->body;
+          $itemWriter->setIdStr($id_str);
+          $itemWriter->setPayload($payload);
+          $itemWriter->setUserId($userId);
+          $itemWriter->setProjectId($projectId);
+          $itemWriter->setLastItem($last_item);
+
+          $itemWriter->writeItem();
+
+
+        } elseif (property_exists($payload->data->body, 'crash_report')) {
+          $itemWriter->setItemType('crash_report');
+
+        }
+
+        if ($itemWriter->isQuerySuccess()) {
+          if ($mysqli->commit()) {
+            header('Content-Type: application/json; charset=utf-8');
+            http_response_code(200);
+            $uuid = $payload->data->uuid;
+            echo "{err: 0,result: {uuid: \"$uuid\"}}";
+          } else {
+            header('Content-Type: application/json; charset=utf-8');
+            http_response_code(500);
+            echo "{\"err\": 1, \"message\": \"DB error: {$itemWriter->getMysqliError()}\"}";
+          }
         } else {
+          $mysqli->rollback();
           header('Content-Type: application/json; charset=utf-8');
           http_response_code(500);
-          echo '{"err": 1, "message": "DB error"}';
+          echo "{\"err\": 1, \"message\": \"DB error: {$itemWriter->getMysqliError()}\"}";
         }
+
+
       } else {
-        $mysqli->rollback();
         header('Content-Type: application/json; charset=utf-8');
         http_response_code(500);
-        echo '{"err": 1, "message": "DB error"}';
+        echo '{"err": 1, "message": "wrong token"}';
       }
 
     } else {
       header('Content-Type: application/json; charset=utf-8');
       http_response_code(500);
-      echo '{"err": 1, "message": "wrong token"}';
+      echo '{"err": 1, "message": "missing payload data"}';
     }
+
+
 
     break;
 
@@ -134,3 +125,6 @@ $content = ob_get_contents();
 $length = strlen($content);
 header('Content-Length: '. $length);
 echo $content;
+
+
+
---
+++ inc/itemWriter.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * @Project: RollbarServer
+ * @User   : mira
+ * @Date   : 26.11.18
+ * @Time   : 2:21
+ */
+
+namespace rollbug;
+
+class itemWriter
+{
+
+  /**
+   * @var \stdClass
+   */
+  private $payload;
+  /**
+   * @var int
+   */
+  private $last_item;
+  /**
+   * @var int
+   */
+  private $userId;
+  /**
+   * @var int
+   */
+  private $projectId;
+  /**
+   * @var string
+   */
+  private $id_str;
+  /**
+   * @var string
+   */
+  private $itemType;
+  /**
+   * @var int
+   */
+  private $firstInChain = 0;
+
+  /**
+   * @var \mysqli
+   */
+  private $mysqli;
+  /**
+   * @var \config
+   */
+  private $config;
+
+  /**
+   * @var bool
+   */
+  private $query_success = true;
+  /**
+   * @var string
+   */
+  private $mysqli_error = '';
+
+  /**
+   * itemWriter constructor.
+   *
+   * @param \mysqli $mysqli
+   * @param \config $config
+   */
+  public function __construct(\mysqli $mysqli, \config $config)
+  {
+    $this->mysqli = $mysqli;
+    $this->config = $config;
+  }
+
+  /**
+   * @param \stdClass $payload
+   */
+  public function setPayload(\stdClass $payload): void
+  {
+    $this->payload = $payload;
+  }
+
+  /**
+   * @param int $last_item
+   */
+  public function setLastItem(int $last_item): void
+  {
+    $this->last_item = $last_item;
+  }
+
+  /**
+   * @param int $userId
+   */
+  public function setUserId(int $userId): void
+  {
+    $this->userId = $userId;
+  }
+
+  /**
+   * @param int $projectId
+   */
+  public function setProjectId(int $projectId): void
+  {
+    $this->projectId = $projectId;
+  }
+
+  /**
+   * @param string $id_str
+   */
+  public function setIdStr(string $id_str): void
+  {
+    $this->id_str = $id_str;
+  }
+
+  /**
+   * @param string $itemType
+   */
+  public function setItemType(string $itemType): void
+  {
+    $this->itemType = $itemType;
+  }
+
+
+  /**
+   * @param int $firstInChain
+   */
+  public function setFirstInChain(int $firstInChain): void
+  {
+    $this->firstInChain = $firstInChain;
+  }
+
+  /**
+   * @return \stdClass
+   */
+  public function getPayload(): \stdClass
+  {
+    return $this->payload;
+  }
+
+  /**
+   * @return int
+   */
+  public function getFirstInChain(): int
+  {
+    return $this->firstInChain;
+  }
+
+  /**
+   * @return bool
+   */
+  public function isQuerySuccess(): bool
+  {
+    return $this->query_success;
+  }
+
+  /**
+   * @return string
+   */
+  public function getMysqliError(): string
+  {
+    return $this->mysqli_error;
+  }
+
+  /**
+   * @param \stdClass $trace
+   *
+   * @return string
+   */
+  public function makeIdStr(\stdClass $trace): string
+  {
+    return $trace->frames[ count($trace->frames) - 1 ]->filename . '|'
+        . $trace->frames[ count($trace->frames) - 1 ]->lineno . '|'
+        . $trace->exception->class . '|'
+        . $trace->exception->message;
+  }
+
+  /**
+   * @param bool $traceChain
+   */
+  public function writeItem(bool $traceChain = false): void
+  {
+
+    // search for item in db
+    $stmt = $this->mysqli->prepare('select item.id, item.last_occ from item where user_id=? and project_id=? and id_str=?');
+    $stmt->bind_param('iis', $this->userId, $this->projectId, $this->id_str);
+    $stmt->bind_result($itemId, $last_occ);
+    $this->query_success = $this->query_success && $stmt->execute();
+    $this->mysqli_error .= "\n" . $this->mysqli->error;
+    $stmt->fetch();
+    $stmt->close();
+
+    if (property_exists($this->payload->data, 'timestamp')) {
+      $timeStamp = $this->payload->data->timestamp;
+    } elseif (property_exists($this->payload->data->body, 'telemetry')) {
+      $timeStamp = (int)($this->payload->data->body->telemetry[0]->timestamp_ms / 1000);
+    }
+
+    $date = new \DateTime('@' . $timeStamp);
+    $timestamp = $date->format('Y-m-d H:i:s');
+
+    // if new item
+    if ($itemId === null) {
+      $itemId = $this->last_item + 1;
+      $stmt = $this->mysqli->prepare('INSERT INTO item (id, user_id, project_id, level, language, id_str, type, last_occ, last_timestamp, first_in_chain) VALUES (?,?,?,?,?,?,?,1, ?,?);');
+      $stmt->bind_param('iiisssssi', $itemId, $this->userId, $this->projectId, $this->payload->data->level, $this->payload->data->language, $this->id_str, $this->itemType, $timestamp, $this->firstInChain);
+      $this->query_success = $this->query_success && $stmt->execute();
+      $this->mysqli_error .= "\n" . $this->mysqli->error;
+      $stmt->fetch();
+      $stmt->close();
+
+      $stmt = $this->mysqli->prepare('UPDATE project SET last_item = last_item + 1 WHERE user_id=? and id=?');
+      $stmt->bind_param('ii', $this->userId, $this->projectId);
+      $this->query_success = $this->query_success && $stmt->execute();
+      $this->mysqli_error .= "\n" . $this->mysqli->error;
+      $stmt->close();
+
+      $last_occ = 1;
+    } else {
+      $stmt = $this->mysqli->prepare('UPDATE item SET last_occ = last_occ + 1, last_timestamp=? WHERE user_id=? and project_id=? and id=?');
+      $stmt->bind_param('siii', $timestamp, $this->userId, $this->projectId, $itemId);
+      $this->query_success = $this->query_success && $stmt->execute();
+      $this->mysqli_error .= "\n" . $this->mysqli->error;
+      $stmt->close();
+
+      $last_occ++;
+    }
+    $this->last_item = $itemId;
+    if ($traceChain){
+      $this->firstInChain = $this->firstInChain === 0 ? $this->last_item : $this->firstInChain;
+    }
+
+    if ($last_occ <= $this->config->max_occurences) {
+      $data = json_encode($this->payload->data);
+      $stmt = $this->mysqli->prepare('INSERT INTO occurence (id, user_id, project_id, item_id, timestamp, data) VALUES (?,?,?,?,?,?);');
+      $stmt->bind_param('iiiiss', $last_occ, $this->userId, $this->projectId, $itemId, $timestamp, $data);
+      $this->query_success = $this->query_success && $stmt->execute();
+      $this->mysqli_error .= "\n" . $this->mysqli->error;
+      $stmt->fetch();
+      $stmt->close();
+    }
+
+    /*
+    $fp = fopen(__DIR__ . "/mysqli.txt", 'wb');
+    fwrite($fp, $mysqli_error);
+    fclose($fp);
+     */
+
+
+  }
+
+}
+
