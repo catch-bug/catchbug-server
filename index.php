94747f5360afa125c6768c19f2671e20752f7020
mira <ms502040@gmail.com>
2018-12-03 16:57:27 +0100

    version in page header

--- index.php
+++ index.php
@@ -252,7 +252,7 @@ HTML;
 // ----------------------------------- navbar
 echo <<<HTML
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="/">Fixed navbar</a>
+      <a class="navbar-brand" href="/">rollBug <small><small class="text-muted">v{$config->version}</small></small></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
